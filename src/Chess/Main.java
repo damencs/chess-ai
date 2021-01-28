3fef79f923c891ee1ab4e9086a6a0d4e60792cd7
JaymeAlann <jaymealann@gmail.com>
2021-01-27 20:41:41 -0500

    File Changes

--- src/Chess/Main.java
+++ src/Chess/Main.java
@@ -1,4 +1,4 @@
-package home.fmxl;
+package Chess;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 900, 450));
--- src/Chess/controllers/Controller.java
+++ src/Chess/controllers/Controller.java
@@ -1,4 +1,4 @@
-package home.controllers;
+package Chess.controllers;
 
 public class Controller {
 }
