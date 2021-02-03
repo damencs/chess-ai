4e8c4736b1d764b87532e14d7761319d3466bf93
bcrea <bcreative.43@gmail.com>
2021-02-03 15:12:07 -0500

    Added comments in the main function.

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -1,8 +1,6 @@
 package chess;
 
-import com.sun.javafx.application.LauncherImpl;
 import javafx.application.Application;
-import javafx.application.Preloader;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,18 +8,41 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import static com.sun.javafx.application.LauncherImpl.launchApplication;
+/*
+    Name: Tyra Buadoo
+    Date: 2-3-2021
+    Desc: A Chess AI variant game for Senior Project
+*/
 
-// Chess Board Examples
-// https://javabook.bloomu.edu/code-html/Chapter6/CheckerBoard.html
-// https://codereview.stackexchange.com/questions/187925/javafx-checkers-game
-// https://github.com/RamyaPayyavula/CheckerBoard
+/**
+ * Learning Material:
+ *
+ * Branches, Commit, Pull Request, Merge in IntelliJ IDEA and GitHub: Example with Code- https://www.youtube.com/watch?v=jEN3D9EN7ss
+ * Chess Board Examples
+        https://javabook.bloomu.edu/code-html/Chapter6/CheckerBoard.html
+        https://codereview.stackexchange.com/questions/187925/javafx-checkers-game
+        https://github.com/RamyaPayyavula/CheckerBoard// Chess Board Examples
+        https://javabook.bloomu.edu/code-html/Chapter6/CheckerBoard.html
+        https://codereview.stackexchange.com/questions/187925/javafx-checkers-game
+        https://github.com/RamyaPayyavula/CheckerBoard
+
+    How to Install Java FX for IntelliJ
+        Setup JavaFX 11, 13, 14 or 15 on IntelliJ - Quick and Easy (2020)- https://www.youtube.com/watch?v=exTe8Bvtrg0
+        (Best explanation so follow this on intead)
+
+        How to Setup IntelliJ 2020.2.2 for JavaFX 15, JDK 15, Scene Builder 11.0.0 on Windows 10 x64- https://www.youtube.com/watch?v=RyTn2zGO0e0
+        (When it adds the path in VM options [if you don't see VM options, click on "modify options" and add it], use the command
+        that this user uses since they are using Windows and the command in the video above is for Mac).
+
+    1 - Setup JavaFX and Scene Builder with IntelliJ IDEA on Windows 10- https://www.youtube.com/watch?v=ElKRqK87YMs
+    (How to include scene builder to this IDE)
+ *
+ * **/
 
 
 public class Main extends Application {
 
 //    private final int COUNT_LIMIT = 500000;
-//
 //    @Override
 //    public void init()throws Exception{
 //        for(int i = 0; i < COUNT_LIMIT; i++){
@@ -44,8 +65,9 @@ public class Main extends Application {
     public static void main(String[] args) {
 
         // Launch Preloader before main
-        //launchApplication(Main.class, SplashScreenMain.class, args);
+        //LauncherImpl.launchApplication(Main.class, SplashScreenMain.class, args);
 
         launch(args);
     }
+
 }
